ed7dceb55433b1fe8f4d6e602810485fcdec5f7a
MikeLrUC <rabuge@student.dei.uc.pt>
2021-04-23 14:29:53 +0100

    Problem B - mooshak version - 200pts

--- src/B/main.cpp
+++ src/B/main.cpp
@@ -3,9 +3,11 @@
 #include <algorithm>
 #include <cmath>
 
+#define ASCENDING (std::size_t) 0
+#define DESCENDING (std::size_t) 1
 #define MODULE 1000000007
 
-std::array<std::array<int, 500>, 60000> dp_matrix;
+std::array<std::array<std::array<int, 2>, 500>, 60000> dp_matrix;
 
 int mod_abs(int a, int mod) {
 	return ((a % mod) + mod) % mod;
@@ -19,48 +21,57 @@ int mod_sub(int a, int b, int mod) {
 	return mod_add(a, -b, mod);
 }
 
-void print_debug(std::size_t n,std::size_t depth) {
+void print_debug(std::size_t n,std::size_t depth, std::size_t mode) {
     for (std::size_t i = 0; i < depth; ++i) {
         for (std::size_t j = 0; j < n; ++j) {
-            std::cout << dp_matrix[i][j] << " ";
+            std::cout << dp_matrix[i][j][mode] << " ";
         }
         std::cout << "\n";
     }
 }
 
 int solve(std::size_t &h, std::size_t &n, std::size_t &depth) {
-    std::size_t i, j, k;
+    std::size_t i, j, lowest;
     int sum = 0;
-    dp_matrix[0][0] = 1;
+    dp_matrix[0][0][ASCENDING] = 1;
 
     // Ascending Step
-    // for (j = 0; j < n - 1; ++j) {
-    //     for (i = j; (i < depth) && dp_matrix[i][j]; ++i) {
-    //         for (k = i + 1; (k < i + h) && (k < depth); ++k) {
-    //             dp_matrix[k][j + 1] = mod_add(dp_matrix[k][j + 1], dp_matrix[i][j], MODULE);
-    //         }
-    //     }
-    // }
-
-    // print_debug(n,depth);
     for (j = 1; j < n; ++j) {
         for (i = j; i < depth; ++i) {
-            dp_matrix[i][j] = mod_add(dp_matrix[i - 1][j], dp_matrix[i - 1][j - 1], MODULE);
-            if (h <= i && dp_matrix[i - h][j - 1]) 
-                dp_matrix[i][j] = mod_sub(dp_matrix[i][j], dp_matrix[i - h][j - 1], MODULE);
+            dp_matrix[i][j][ASCENDING] = mod_add(dp_matrix[i - 1][j][ASCENDING], dp_matrix[i - 1][j - 1][ASCENDING], MODULE);
+            if (h <= i && dp_matrix[i - h][j - 1][ASCENDING]) 
+                dp_matrix[i][j][ASCENDING] = mod_sub(dp_matrix[i][j][ASCENDING], dp_matrix[i - h][j - 1][ASCENDING], MODULE);
         }
     }
+
     // Descending Step
     for (j = 2; j < n; ++j) {
-        for (i = 0; (i <= (h - 2) + (h - 1) * (j-2)) && (i < depth); ++i) {
-            for (k = i + 1; (k < i + h) && (k < depth); ++k) {
-                dp_matrix[i][j] = mod_add(dp_matrix[i][j], dp_matrix[k][j - 1], MODULE);
+        lowest = std::min((h - 2) + (h - 1) * (j-2), depth - 2);
+        dp_matrix[lowest][j][DESCENDING] = mod_add(dp_matrix[lowest][j][ASCENDING], dp_matrix[lowest + 1][j - 1][ASCENDING], MODULE);
+        if (lowest > 0) {
+            for (i = lowest - 1;; --i) {
+                if (dp_matrix[i + 1][j - 1][DESCENDING])
+                    dp_matrix[i][j][DESCENDING] = mod_add(dp_matrix[i + 1][j][DESCENDING], dp_matrix[i + 1][j - 1][DESCENDING], MODULE);
+                else 
+                    dp_matrix[i][j][DESCENDING] = mod_add(dp_matrix[i + 1][j][DESCENDING], dp_matrix[i + 1][j - 1][ASCENDING], MODULE);
+                if (i >= j)
+                    dp_matrix[i][j][DESCENDING] = mod_add(dp_matrix[i][j][DESCENDING], dp_matrix[i][j][ASCENDING], MODULE);
+                if (i + 1 >= j)
+                    dp_matrix[i][j][DESCENDING] = mod_sub(dp_matrix[i][j][DESCENDING], dp_matrix[i + 1][j][ASCENDING], MODULE);
+                if (i + h < depth) {
+                    if (dp_matrix[i + h][j - 1][DESCENDING])
+                        dp_matrix[i][j][DESCENDING] = mod_sub(dp_matrix[i][j][DESCENDING], dp_matrix[i + h][j - 1][DESCENDING], MODULE);
+                    else
+                        dp_matrix[i][j][DESCENDING] = mod_sub(dp_matrix[i][j][DESCENDING], dp_matrix[i + h][j - 1][ASCENDING], MODULE);
+                }
+                if (i == 0) break;
             }
         }
-    }   
+    }
+
     // Sum
     for (j = 2; j < n; ++j) {
-        sum = mod_add(sum, dp_matrix[0][j], MODULE);
+        sum = mod_add(sum, dp_matrix[0][j][DESCENDING], MODULE);
     }
     return sum;
 
@@ -70,20 +81,20 @@ int main(void) {
 	std::ios::sync_with_stdio(false);
 	std::cin.tie(nullptr);
 	std::cout.tie(nullptr);
-
 	std::size_t depth, tests, n, h, H;
 
     std::cin >> tests;
     while(tests--) {
         std::cin >> n >> h >> H;
-        if (H < h) {
+        if (H < h || h < 2) {
             std::cout << "0\n";
             continue;
         }
         depth = std::min(H - h, (h - 1) * (n - 1 - (n / 2))) + 1;
-        // std::cout << "Old Depth: " << H - h << "\tCurrent Depth: " << depth - 1 << "\t";
-        for (std::size_t i = 0; i < depth; ++i){ 
-            std::fill(dp_matrix[i].begin(), dp_matrix[i].begin() + n, 0);
+        for (std::size_t i = 0; i < depth; ++i){
+            for(std::size_t j = 0; j < n; ++j){
+                std::fill(dp_matrix[i][j].begin(), dp_matrix[i][j].end(), 0);
+            }
         }
         std::cout << solve(h, n, depth) << "\n";
     }
